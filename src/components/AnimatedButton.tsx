0c2f7e8903a1fdb4c2302a1cd901505f55e16f7e
Duncan Maina <duncandevs@gmail.com>
2025-04-07 21:35:37 -0500

    reduce animated button file complexity

--- src/components/AnimatedButton.tsx
+++ src/components/AnimatedButton.tsx
@@ -1,7 +1,7 @@
-// styles.stylex.ts
+import * as React from "react";
 import * as stylex from "@stylexjs/stylex";
 
-export const styles = stylex.create({
+const styles = stylex.create({
   container: {
     position: "relative",
     width: 400,
@@ -57,3 +57,39 @@ export const styles = stylex.create({
     },
   },
 });
+
+type AnimatedButtonProps = {
+  title: string;
+};
+export const AnimatedButton: React.FC<AnimatedButtonProps> = ({ title }) => {
+  const [hovered, setHovered] = React.useState(false);
+
+  return (
+    <div
+      {...stylex.props(styles.container)}
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+    >
+      <div
+        {...stylex.props(
+          styles.rect,
+          styles.posOne,
+          hovered ? styles.down : styles.top,
+          hovered ? styles.secondary : styles.primary,
+        )}
+      >
+        <span {...stylex.props(styles.content)}>
+          <p className="font-sans-regular">{title}</p>
+        </span>
+      </div>
+      <div
+        {...stylex.props(
+          styles.rect,
+          styles.posTwo,
+          hovered ? styles.top : styles.down,
+          hovered ? styles.primary : styles.secondary,
+        )}
+      />
+    </div>
+  );
+};
